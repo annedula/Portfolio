178c32f938f6761ad6e6fac1ffdde211d1c40e1b
annedla <jennyannedula4@gmail.com>
2025-07-18 18:35:27 +0800

    Updated htmll and js

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ var popoverTriggerList = [].slice.call(
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl);
 });
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("gallery-item")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("gallery-modal")
+    );
+    myModal.show();
+  }
+});
